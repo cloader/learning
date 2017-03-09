e907320bd1c59a37c04ac7af3369e0dbba23bd78
chen <cg3940@qq.com>
2017-03-09 14:02:27 +0800

    添加静态资源测试

--- springboot/springboot.Chapter1/README.md
+++ springboot/springboot.Chapter1/README.md
@@ -1,1 +1,3 @@
-Hello Word!
+搭建一个脚手架测试Hello Word!
+在`/resources/static`下放入一张图片，测试SpringBoot的默认静态资源访问`localhost://8080/a.png`
+
--- springboot/springboot.Chapter1/src/test/java/chapter1/Chapter1ApplicationTests.java
+++ springboot/springboot.Chapter1/src/test/java/chapter1/Chapter1ApplicationTests.java
@@ -1,5 +1,10 @@
 package chapter1;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,15 +14,12 @@ import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import springboot.chapter1.web.HelloController;
 
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 /*
@@ -30,12 +32,23 @@ public class Chapter1ApplicationTests {
 	private MockMvc mvc;
 	@Before
 	public void setUp() throws Exception {
+		//mvc=MockMvcBuilders.webAppContextSetup(new StaticWebApplicationContext()).build();
 		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
 	}
 	@Test
 	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+		mvc.perform(get("/hello").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Hello World")));
 	}
+	
+	
+	/*
+	 * 不能测试静态资源
+	 */
+	@Test
+	public void getPng() throws Exception{
+		mvc.perform(get("/a.png")).andExpect(status().isOk());
+	}
+	
 }
